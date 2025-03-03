e9495d20d39bb091e44af975949bcce4a6d44928
warmann0 <mnw@warmann.org>
2025-03-02 22:53:42 -0700

    debuging

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const Customer = require('./models/customer');
+
 const prompt = require('prompt-sync')();
 
-const username = prompt('What is your name? ');
+// const username = prompt('What is your name? ');
 
-console.log(`Your name is ${username}`);
+// console.log(`Your name is ${username}`);
+
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+
+    await runQueries();
+
+    welcome();
+
+    await mongoose.disconnect();
+    console.log('Disconnected from MongoDB');
+
+    process.exit();
+};
